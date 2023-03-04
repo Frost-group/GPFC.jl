f7113c5bf06f063cc85a61808763c8ccc5e6de4d
Keerati  Keeratikarn <77400504+PKKeerati@users.noreply.github.com>
2023-03-04 18:14:31 +0000

    Update GPFC.jl

--- src/GPFC.jl
+++ src/GPFC.jl
@@ -1,7 +1,6 @@
 
 module GPFC
 
-<<<<<<< HEAD
 # Kernel Functions
 using KernelFunctions
 # Automatic differentiation
@@ -13,15 +12,6 @@ using CSV, DataFrames
 # Optimization 
 using Optim
 
-=======
-
-using KernelFunctions, ForwardDiff
-using Einsum, LinearAlgebra
-using CSV
-using DataFrames
-using Optim
-using Zygote
->>>>>>> c8895da47bb2758f8d880527221306c496057723
 using Distributed
 # Writing FC2 + FC3 for phononpy
 using HDF5
